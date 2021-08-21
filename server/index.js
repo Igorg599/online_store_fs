cb876428f37e8b329c9fde429d4063876554c2f9
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2021-08-21 11:01:24 +0500

    connect db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 require("dotenv").config() // для доступ к переменным окружения
 const express = require("express")
+const sequelize = require("./db")
 
 const app = express()
 
 const PORT = process.env.PORT || 4004
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+const start = async () => {
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync()
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
