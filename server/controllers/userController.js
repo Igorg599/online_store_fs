8da276b9e754b3df8a296b8ba08aaeb93d44f0c9
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2021-09-02 10:49:54 +0500

    login server

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -28,7 +28,21 @@ class UserController {
     return res.json({ token })
   }
 
-  async login(req, res) {}
+  async login(req, res, next) {
+    const { email, password } = req.body
+    const user = await User.findOne({ where: { email } })
+    if (!user) {
+      return next(
+        ApiError.internal("Пользователь с таким email не существует!")
+      )
+    }
+    let comparePassword = bcrypt.compare(password, user.password)
+    if (!comparePassword) {
+      return next(ApiError.internal("Указан неверный пароль"))
+    }
+    const token = generateJwt(user.id, user.email, user.role)
+    return res.json({ token })
+  }
 
   async check(req, res, next) {
     const { id } = req.query
