31e02980605829b8d1838d622f15a7cbad082897
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2021-09-03 17:50:40 +0500

    add appRouter

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,7 +1,21 @@
 import { Switch, Route, Redirect } from "react-router-dom"
+import { authRoutes, publicRoutes } from "../routes"
+import { SHOP_ROUTE } from "../utils/consts"
 
 const AppRouter = () => {
-  return <div></div>
+  const isAuth = false
+  return (
+    <Switch>
+      {isAuth === true &&
+        authRoutes.map(({ path, Component }) => (
+          <Route key={path} path={path} component={Component} exact />
+        ))}
+      {publicRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} component={Component} exact />
+      ))}
+      <Redirect to={SHOP_ROUTE} />
+    </Switch>
+  )
 }
 
 export default AppRouter
